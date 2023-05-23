04177a0a56b7211172767387d7231dcf78f147a8
rrnayak70 <rishabhnayak2002@gmail.com>
2023-05-24 01:49:32 +0530

    24-05-2023

--- routes/pool.js
+++ routes/pool.js
@@ -1,10 +1,11 @@
 var mysql=require('mysql')
 var pool=mysql.createConnection({
-host:'localhost',
-port:3306,
+host:'aws.connect.psdb.cloud',
+// port:3306,
 database:'paynrent',
-user:'root',
-password:'2003',
+user:'rtk2rapqc7f8pamyo8p3',
+password:'pscale_pw_ClMaI5Bnwzsa6REG5TagRTgrLaakRhMhWvb0EfV8K15',
 multipleStatements:true,
 })
+// console.log("connected");
 module.exports=pool

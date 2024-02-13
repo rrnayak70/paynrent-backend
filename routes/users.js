d14d967842f35955cc32c68860185e0433830794
rrnayak70 <rishabhnayak2002@gmail.com>
2024-02-14 00:31:25 +0530

    test

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
+var pool = require('./pool')
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  // res.send('respond with a resource');
+    pool.query('select * from administrator',function(error,result){
+      if(!error){
+        res.status(200).json({data:result})
+      }
+    })
 });
 
 module.exports = router;

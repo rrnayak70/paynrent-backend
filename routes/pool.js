8255fc6daf1a47c90972e4a5604848cd9d629839
rrnayak70 <87633076+rrnayak70@users.noreply.github.com>
2024-02-13 23:26:21 +0530

    Update pool.js
    stablish mysql cloud connection

--- routes/pool.js
+++ routes/pool.js
@@ -1,10 +1,10 @@
 var mysql=require('mysql')
 var pool=mysql.createConnection({
-host:'aws.connect.psdb.cloud',
+host:'rishi.chukok6gc7j1.us-east-1.rds.amazonaws.com',
 // port:3306,
 database:'paynrent',
-user:'rtk2rapqc7f8pamyo8p3',
-password:'pscale_pw_ClMaI5Bnwzsa6REG5TagRTgrLaakRhMhWvb0EfV8K15',
+user:'rrnayak70',
+password:'9302877691',
 multipleStatements:true,
 })
 // console.log("connected");
